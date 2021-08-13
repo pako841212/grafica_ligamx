be62734d84207db8eea584b0078817c4457eeab5
pako841212 <80023806+pako841212@users.noreply.github.com>
2021-08-12 17:25:55 -0700

    Create plot_xPuntos.R

---
+++ plot_xPuntos.R
@@ -0,0 +1,12 @@
+setwd("C:/Users/Francisco/OneDrive - Instituto Tecnologico y de Estudios Superiores de Monterrey/Doctorado - COLSON/Semestre 2/EACS/Prácticas R")
+getwd()
+
+ligamx<-read.csv("C:/Users/Francisco/OneDrive - Instituto Tecnologico y de Estudios Superiores de Monterrey/Doctorado - COLSON/Semestre 2/EACS/Prácticas R/3ra.csv")
+
+plot(ligamx$xPuntos,ligamx$puntos)
+plot(ligamx$xPuntos,ligamx$puntos,type="l")
+X<-c(0,10)
+Y<-c(0,10)
+plot(ligamx$xPuntos,ligamx$puntos,xlim=c(1, 10),ylim=c(1, 10),xlab="xPuntos", ylab="PUNTOS ")
+lines(X,Y,type="l")
+text(puntos+0.5~xPuntos, labels=(id),ligamx, cex=0.9, font=1)
