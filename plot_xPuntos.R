77dc59c34e9bc6a88bf52958606a1c9687d4dc23
pako841212 <80023806+pako841212@users.noreply.github.com>
2021-08-12 17:28:24 -0700

    dale espacio

--- plot_xPuntos.R
+++ plot_xPuntos.R
@@ -5,8 +5,8 @@ ligamx<-read.csv("C:/Users/Francisco/OneDrive - Instituto Tecnologico y de Estud
 
 plot(ligamx$xPuntos,ligamx$puntos)
 plot(ligamx$xPuntos,ligamx$puntos,type="l")
-X<-c(0,10)
-Y<-c(0,10)
+X <- c(0,10)
+Y <- c(0,10)
 plot(ligamx$xPuntos,ligamx$puntos,xlim=c(1, 10),ylim=c(1, 10),xlab="xPuntos", ylab="PUNTOS ")
 lines(X,Y,type="l")
 text(puntos+0.5~xPuntos, labels=(id),ligamx, cex=0.9, font=1)
